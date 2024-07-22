a5f6104c7c864b31368f2ed07e9ff99b224f838f
EyadGamer1757 <166323143+EyadGamer1757@users.noreply.github.com>
2024-07-23 02:07:12 +0300

    Add files via upload

---
+++ eyad.js
@@ -0,0 +1,78 @@
+document.write("<h1>Made by Moayad Ashraf Elkolfat</h1>")
+let a;
+if (localStorage.name == null){
+    a = prompt('Enter Your Name:');
+    localStorage.setItem('name', a);
+    alert('The name that you entered is: ' + a);
+}else{
+    a = localStorage.getItem('name');
+}
+if (a.toLowerCase() == '(ad)') {
+    document.write('<h1>Hello Admin Moayad😁!</h1>');
+    document.write("<h1>blox fruit scripts</h1>");
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()");</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main/Script<"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>getgenv().WaterMark = trueloadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()</h3>');
+    document.write("<h2> the end rest of the scripts will be added soon!</h2>");
+} else if (a.toLowerCase() == 'friend') {
+    document.write('<h1>Hello our friend😁?</h1>');
+    document.write("<h1>blox fruit scripts</h1>");
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()");</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main/Script<"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>getgenv().WaterMark = trueloadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()</h3>');
+    document.write("<h2> the end rest of the scripts will be added soon!</h2>");
+}
+else if (a.toLowerCase() == "plsdonate" || a.toLowerCase() == "pls donate") {
+    document.write("<h1>🤬امشي يا ابن الكلب <h1>");
+}
+else if (a == '') {
+    document.write(`<h1>Hello Unknown!</h1>`);
+    document.write("<h1>blox fruit scripts</h1>");
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()");</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main/Script<"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>getgenv().WaterMark = trueloadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()</h3>');
+    document.write("<h2> the end rest of the scripts will be added soon!</h2>");
+}
+else {
+    document.write(`<h1>Hello ${a}😁!</h1>`);
+    document.write("<h1>blox fruit scripts</h1>");
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()");</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main/Script<"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>getgenv().WaterMark = trueloadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()</h3>');
+    document.write('<h4>Auto farm level script:-</h4>');
+    document.write('<h3>loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()</h3>');
+    document.write("<h2>end! rest of the scripts will be added soon!</h2>");
+}
+function rename(){
+    localStorage.clear();
+    window.location.reload();
+}
+if(a.toLowerCase() == "boudy" || a.toLowerCase() == "body"){
+    console.error("err:don't enter the website again");
+}
